4125accb00258af2f8a2b9aa4f488d0c02473634
Audrea Cook <audrea.cook@dutchie.com>
2023-04-17 10:52:48 -0400

    Add DbContext

---
+++ Data/NZWalksDbContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Data;
+
+public class NZWalksDbContext: DbContext
+{
+    public NZWalksDbContext(DbContextOptions dbContextOptions): base(dbContextOptions)
+    {
+        
+    }
+
+    public DbSet<Difficulty> Difficulties { get; set; } // DbSet is a collection of entities
+    public DbSet<Region> Regions { get; set; }
+    public DbSet<Walk> Walks { get; set; }
+    
+}
