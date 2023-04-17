08cbbc593b5638ac87dbbe51d36c00a4d161b0da
Audrea Cook <audrea.cook@dutchie.com>
2023-04-17 15:19:36 -0400

    Add get region by id

--- Controllers/RegionsController.cs
+++ Controllers/RegionsController.cs
@@ -1,30 +1,42 @@
 using Microsoft.AspNetCore.Mvc;
 using NZWalks.API.Data;
-using NZWalks.API.Models.Domain;
 
-namespace NZWalks.API.Controllers
+namespace NZWalks.API.Controllers;
+
+[Route("api/[controller]")] // api/Regions
+[ApiController]
+public class RegionsController : Controller
 {
-    [Route("api/[controller]")] // api/Regions
-    [ApiController]
-    
-    public class RegionsController : Controller
+    private readonly NZWalksDbContext dbContext;
+
+    public RegionsController(NZWalksDbContext dbContext)
     {
-        private readonly NZWalksDbContext dbContext;
-        
-        public RegionsController(NZWalksDbContext dbContext)
-        {
-            this.dbContext = dbContext;
-        }
-        
-        // GET all regions
-        // GET: https://localhost:5001/api/Regions
-        [HttpGet]
-        public IActionResult GetAll()
-        {
-            // Get all regions from the database
-            var regions = dbContext.Regions.ToList();
-            
-            return Ok(regions);
-        }
+        this.dbContext = dbContext;
+    }
+
+    // GET all regions
+    // GET: https://localhost:5001/api/Regions
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        // Get all regions from the database
+        var regions = dbContext.Regions.ToList();
+
+        return Ok(regions);
+    }
+
+    // GET a region by id
+    // GET: https://localhost:5001/api/Regions/1
+    [HttpGet]
+    [Route("{id:guid}")]
+    public IActionResult GetById([FromRoute] Guid id)
+    {
+        // Get a region from the database
+        var region = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+
+        // If the region is null, return a 404
+        if (region == null) return NotFound();
+
+        return Ok(region);
     }
 }
